725f8a036c7258cdd02e8bd0695035e8642405df
anys34 <anyeseong34@gmail.com>
2023-12-31 23:28:18 +0900

    ADD :: 9.2.3 리프레시 토큰 도메인 구현

---
+++ src/main/java/com/anys34/oauth2/repository/RefreshTokenRepository.java
@@ -0,0 +1,11 @@
+package com.anys34.oauth2.repository;
+
+import com.anys34.oauth2.domain.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByUserId(Long userId);
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+}
