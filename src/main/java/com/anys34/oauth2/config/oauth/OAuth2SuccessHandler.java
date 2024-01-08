54ec7fcdefc923195de552b9e3e7d35f3007515d
anys34 <anyeseong34@gmail.com>
2024-01-08 11:53:00 +0000

    DELETE :: articles

--- src/main/java/com/anys34/oauth2/config/oauth/OAuth2SuccessHandler.java
+++ src/main/java/com/anys34/oauth2/config/oauth/OAuth2SuccessHandler.java
@@ -24,7 +24,7 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     public static final String REFRESH_TOKEN_COOKIE_NAME = "refresh_token";
     public static final Duration REFRESH_TOKEN_DURATION = Duration.ofDays(14);
     public static final Duration ACCESS_TOKEN_DURATION = Duration.ofDays(1);
-    public static final String REDIRECT_PATH = "/articles";
+    public static final String REDIRECT_PATH = "/";
 
     private final TokenProvider tokenProvider;
     private final RefreshTokenRepository refreshTokenRepository;
--- src/main/java/com/anys34/oauth2/repository/BlogRepository.java
+++
@@ -1,8 +0,0 @@
-package com.anys34.oauth2.repository;
-
-import com.anys34.oauth2.domain.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogRepository extends JpaRepository<Article, Long> {
-}
-
