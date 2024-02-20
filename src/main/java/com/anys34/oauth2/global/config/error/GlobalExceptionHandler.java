e8061b5a7796a8402f4e9869855e288cf5a3ba8c
anys34 <anyeseong34@gmail.com>
2024-02-20 23:55:10 +0900

    error

--- src/main/java/com/anys34/oauth2/global/config/error/GlobalExceptionHandler.java
+++ src/main/java/com/anys34/oauth2/global/config/error/GlobalExceptionHandler.java
@@ -3,41 +3,16 @@ package com.anys34.oauth2.global.config.error;
 import com.anys34.oauth2.global.config.error.exception.BusinessException;
 import com.anys34.oauth2.global.config.error.exception.ErrorCode;
 import com.anys34.oauth2.global.config.error.exception.ErrorResponse;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e) {
         ErrorCode errorCode = e.getErrorCode();
         ErrorResponse response = ErrorResponse.of(errorCode);
-
         return new ResponseEntity<>(response, errorCode.getStatus());
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, Map<String, String>>> handleBadRequestExceptions(MethodArgumentNotValidException e) {
-        Map<String, String> errors = e.getBindingResult().getFieldErrors()
-                .stream()
-                .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
-
-        return new ResponseEntity<>(getErrorsMap(errors), HttpStatus.BAD_REQUEST);
-    }
-
-    private Map<String, Map<String, String>> getErrorsMap(Map<String, String> errors) {
-        Map<String, Map<String, String>> errorResponse = new HashMap<>();
-        errorResponse.put("errors", errors);
-        return errorResponse;
     }
 }
--- src/main/java/com/anys34/oauth2/global/config/error/exception/ErrorCode.java
+++ src/main/java/com/anys34/oauth2/global/config/error/exception/ErrorCode.java
@@ -8,18 +8,9 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
 
-    USER_NOT_MATCH(HttpStatus.BAD_REQUEST, "User Not Match"),
-    UNEXPECTED_TOKEN(HttpStatus.BAD_REQUEST, "Unexpected token"),
-    EXPIRED_JWT(HttpStatus.UNAUTHORIZED, "Expired Jwt"),
-    INVALID_JWT(HttpStatus.UNAUTHORIZED, "Invalid Jwt"),
-    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "Invalid Refresh Token"),
-
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User Not Found"),
-    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "Post Not Found"),
-    REFRESH_TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "Refresh Token Not Found"),
-    CONTENT_TYPE_NOT_FOUND(HttpStatus.NOT_FOUND, "Content Type Not Found"),
-
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error");
+    UNEXPECTED_TOKEN(HttpStatus.BAD_REQUEST, "Unexpected token"),
+    EXPIRED_JWT(HttpStatus.UNAUTHORIZED, "Expired Jwt");
 
     private final HttpStatus status;
     private final String message;
